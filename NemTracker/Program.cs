14b1f315e0045a3b2ad9d714cc7696f8c6d0e167
Owen Holloway <owen.holloway@lanceolata.com.au>
2022-03-13 01:02:32 +1100

    Base C# Files

---
+++ NemTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NemTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
