f8f60c70c89058d13a24bdf720d2cb89b2835556
Owen Holloway <owen.holloway@lanceolata.com.au>
2022-03-20 13:30:13 +1100

    Calculate times

---
+++ NemTracker/Features/TimeTools.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace NemTracker.Features
+{
+    public static class TimeTools
+    {
+        public static long NextNemInterval(this DateTime dateTime)
+        {
+
+            dateTime = dateTime.AddSeconds( -1 * dateTime.Second);
+            dateTime = dateTime.AddMilliseconds( -1 * dateTime.Millisecond);
+            
+            if (dateTime.Minute > 55)
+            {
+                var minutes = dateTime.Minute;
+                var minuteDelta = 60 - minutes;
+                var secondDelta = minuteDelta * 60;
+                dateTime = dateTime.AddSeconds(secondDelta);
+            }
+            else
+            {
+                var minuteDelta = 5 - (dateTime.Minute % 5);
+                var secondDelta = minuteDelta * 60;
+                dateTime = dateTime.AddSeconds(secondDelta);
+            }
+            
+            var year = dateTime.Year;
+            var month = dateTime.Month;
+            var day = dateTime.Day;
+            var hour = dateTime.Hour;
+            var minute = dateTime.Minute;
+
+            var result = year.ToString("0000") +
+                         month.ToString("00") +
+                         day.ToString("00") +
+                         hour.ToString("00") +
+                         minute.ToString("00");
+
+            return Int64.Parse(result);
+        }
+        
+        /*
+        public static int ThisNemInterval(this DateTime dateTime)
+        {
+            
+        }*/
+        
+    }
+}
