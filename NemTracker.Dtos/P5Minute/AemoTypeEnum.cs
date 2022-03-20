b80721ed3810a98199c3f269983f4182b1416854
Owen Holloway <owen.holloway@lanceolata.com.au>
2022-03-20 14:06:27 +1100

    Aemo Datatypes move

---
+++ NemTracker.Dtos/P5Minute/AemoTypeEnum.cs
@@ -0,0 +1,11 @@
+namespace NemTracker.Dtos.P5Minute
+{
+    public enum AemoTypeEnum
+    {
+        LocalPrice = 1,
+        CaseSolution = 2,
+        InterconnectorSolution = 4,
+        ConstraintSolution = 6,
+        RegionSolution = 7
+    }
+}
